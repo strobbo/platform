8e1926e79845fec6db5ef1eeca2a0db352af56de
Alan Cardoso <xmballd@gmail.com>
2013-08-15 21:39:48 -0300

    merge do model event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
-	include PublicActivity::Common
+  include PublicActivity::Common
 
-  attr_accessible :date, :description, :location, :name, :time, :user_id
+  attr_accessible :date, :description, :name, :time, :user_id, :city_id
   belongs_to :user
+  belongs_to :city
 end
